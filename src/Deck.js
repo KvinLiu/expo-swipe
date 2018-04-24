891f52560e89ba37bc6a5eb398205baa9ac9cfb3
Kevin <kevin07liu@gmail.com>
2018-04-24 19:07:37 +0600

    feat: Add Default Props fof avoiding error messaging.

--- src/Deck.js
+++ src/Deck.js
@@ -11,6 +11,11 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+  }
+
   constructor(props) {
     super(props);
 
@@ -34,7 +39,7 @@ class Deck extends Component {
 
     // this.panResponder = panResponder;
     // this.position = position;
-    this.state = { panResponder, position };
+    this.state = { panResponder, position, index: 0 };
   }
 
   forceSwipe(direction) {
@@ -42,7 +47,14 @@ class Deck extends Component {
     Animated.timing(this.state.position, {
       toValue: { x, y: 0},
       duration: SWIPE_OUT_DURATION
-    }).start();
+    }).start(()=> this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeRight, onSwipeLeft, data } = this.props;
+    const item = data[this.state.index];
+
+    direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
   }
 
   resetPosition() {
