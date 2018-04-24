3821f3aa53c87223c6b8ec8389dfe0f1896396f7
Kevin <kevin07liu@gmail.com>
2018-04-24 19:17:55 +0600

    feat: Swipe Card functionality is done.

--- src/Deck.js
+++ src/Deck.js
@@ -55,6 +55,10 @@ class Deck extends Component {
     const item = data[this.state.index];
 
     direction === 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+
+    this.state.position.setValue({ x: 0, y: 0});
+
+    this.setState({ index: this.state.index + 1 });
   }
 
   resetPosition() {
@@ -77,8 +81,11 @@ class Deck extends Component {
 
   renderCards() {
     const { panResponder } = this.state;
-    return this.props.data.map((item, index) => {
-      if (index === 0) {
+    return this.props.data.map((item, i) => {
+      if (i < this.state.index) {
+        return null;
+      }
+      if (i === this.state.index) {
         return (
           <Animated.View
             key={item.id}
